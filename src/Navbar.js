8b89a43687e32673da871318a482ada337fd1897
Marvelous rose <marvelousrhose@gmail.com>
2023-04-23 23:12:34 +0100

    updt

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,12 +1,12 @@
 import React from 'react';
-// import logo from './img/logo.png';
+import logo from './img/logo.svg';
 
 const Navbar = () => {
   return (
  
     <nav className="navbar navbar-expand-xl navbar-dark fixed-top">
       <div className="container m-3">
-         <a href="./" className="navbar-brand fw-bold"><img src="" alt="logo"/></a>
+         <a href="./" className="navbar-brand fw-bold mt-4"><img src={logo} alt="logo"/></a>
 
         <button className="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#navbarOffcanvas" aria-controls="navbarOffcanvas" aria-expanded="false" aria-label="Toggle navigation">
           <span className="toggler-icon top-bar"></span>
@@ -17,7 +17,7 @@ const Navbar = () => {
         <div className="offcanvas offcanvas-end" id="navbarOffcanvas" tabIndex="-1" aria-labelledby="offcanvasNavbarLabel" >
           <div className="offcanvas-header">
             <h5 className="offcanvas-title text-dark" id="offcanvasNavbarLabel">
-              <a href="./" className="navbar-brand fw-bold"> <img src="" alt="logo" /> </a>
+              <a href="./" className="navbar-brand fw-bold"> <img src={logo} alt="logo" /> </a>
             </h5>
             <button type="button" className="btn-close btn-close-black" data-bs-dismiss="offcanvas"  aria-label="Close" ></button>
           </div>
@@ -27,7 +27,7 @@ const Navbar = () => {
               <a className="text-dark nav-item nav-link" href="#search">Search</a>
               <a className="text-dark nav-item nav-link" href="#properties">Properties</a>
               <a className="text-dark nav-item nav-link" href="#stories">Stories</a>
-              <a className="text-dark nav-item nav-link" rel='' href="./">Sign-In / Sign-Up</a>
+              <a className=" sign-in text-dark nav-item nav-link" rel='' href="./">Sign-In / Sign-Up</a>
 
            
             </div>
